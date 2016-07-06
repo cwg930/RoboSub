2965aff5a748a1b5fdadcc19c8821c7310fa6914
Azimath <npnickster@gmail.com>
2016-07-06 18:31:41 -0400

    Added bringup package and vision prototype stuff

---
+++ ucf_sub/src/sub_vision/src/bouyfinder.py
@@ -0,0 +1,63 @@
+import numpy as np
+import cv2
+import image_geometry
+
+import vision_utils
+
+from sub_vision.msg import TrackObjectFeedback
+
+class BuoyFinder:
+    def __init__(self, cameraModelLeft, cameraModelStereo, thresholds=Thresholds(upper=(40,52,120), lower=(20, 30, 80))):
+        self.cameraModel = cameraModelLeft
+        self.cameraModelStereo = cameraModelStereo
+        self.thresholds = thresholds
+        
+    def process(self, imageLeftRect, imageRightRect, imageDisparityRect):
+    
+        imageHSV = cv2.cvtColor(imageLeftRect, cv2.COLOR_BGR2HSV)
+        contours, _ = ThreshAndContour(image, thresholds)
+        if len(contours) > 0:
+            circles = filter(lambda x : contourCircularity(x)>0.7, contours)
+        else:
+            return None
+            
+        if len(circles) > 0:
+            biggestCircle = greatestAreaContour(circles)
+        else:
+            return None
+            
+        if cv2.contourArea(biggestCircle) < 300:
+            return None
+            
+        (x,y), radius = cv2.minEnclosingCircle(biggestCircle)
+        _,_,w,h = cv2.boundingRect(biggestCircle)
+        
+        closest = 0
+        
+        if cameraModelStereo is not None:
+            mask = np.zeros((imageDisparityRect.height, imageDisparityRect.width, 1), np.uint8)
+            cv2.circle(mask, (x,y), radius, (255), -1)
+            masked = cv2.bitwise_and(imageDisparityRect, imageDisparityRect, mask=mask)
+            
+            closest = masked.max()
+        
+        if closest > 0: #Get position based off stereo disparity (assumed(?) more accurate)
+            estLocation = self.cameraModelStereo.projectPixelTo3d((x,y),closest)
+        else:           #Get position based off of object size and focal length
+            c = 1 #actual bouy size in units
+            dist = ((c*self.cameraModel.fx())/w + (c*self.cameraModel.fy())/h)/2
+            projectedRay = self.cameraModel.projectPixelTo3dRay((x,y))
+            estLocation = tuple(dist*x for x in projectedRay) #http://stackoverflow.com/a/1781987
+        
+        feedback = TrackObjectFeedback()
+        feedback.fixType=feedback.position
+        feedback.targetPose.Point.x = estLocation[0]
+        feedback.targetPose.Point.y = estLocation[1]
+        feedback.targetPose.Point.z = estLocation[2]
+        
+        feedback.targetPose.Quaternion.w = 1
+        feedback.targetPose.Quaternion.x = 0
+        feedback.targetPose.Quaternion.x = 0
+        feedback.targetPose.Quaternion.x = 0
+        
+        return feedback
---
+++ ucf_sub/src/sub_vision/src/gatefinder.py
@@ -0,0 +1,109 @@
+import numpy as np
+import cv2
+
+import vision_utils
+
+from sub_vision.msg import TrackObjectFeedback
+
+def angle_cos(p0, p1, p2):
+    d1, d2 = (p0-p1).astype('float'), (p2-p1).astype('float')
+    return abs( np.dot(d1, d2) / np.sqrt( np.dot(d1, d1)*np.dot(d2, d2) ) )
+    
+class GateFinder:
+    def __init__(self, stereoCameraModel):
+        self.stereoCameraModel = stereoCameraModel
+        
+    def normalsFromAllCorners(corners, disparities):
+        valid = []
+        for idx, val in enumerate(disparites):
+            if val > 0:
+                valid.append(idx)
+        
+        combos = cartesian(valid, valid, valid)
+        
+        normals = []
+        for combo in combos: #Find all possible cross products of the available points
+            if combo[0] != combo[1] and combo[1] != combo[2] and combo[0] != combo[2]:
+                new = np.cross(corners[combo[1]] - corners[combo[0], corners[combo[2]] - corners[combo[0]))
+                
+                if np.dot(new, np.array([0,0,-1])) < 0:
+                    normals.append(-new)
+                else:
+                    normals.append(new)
+        
+        return normals
+        
+    def process(self, imageLeftRect, imageRightRect, imageDisparityRect):
+    
+        imageHSV = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        contours, _ = ThreshAndContour(image, Thresholds(upper=(40,52,120), lower=(20, 30, 80))
+        
+        if len(contours) == 0:
+            return None
+            
+        rects = []
+        for contour in contours: #adapted from https://github.com/opencv/opencv/blob/master/samples/python/squares.py
+            epsilon = cv2.arcLength(contour, True)*0.05
+            contour = cv2.approxPolyDP(contour, epsilon, True)
+            if len(contour) == 4 and cv2.isContourConvex(contour):
+                contour = contour.reshape(-1, 2)
+                max_cos = np.max([angle_cos( contour[i], contour[(i+1) % 4], contour[(i+2) % 4] ) for i in xrange(4)])
+                if max_cos < 0.1:
+                    rects.append(contour)
+        
+        feedback = TrackObjectFeedback()
+        if len(rects) > 1:
+            rects = greatestNAreaContours(rects, 2)
+            rect1 = cv2.minAreaRect(rects[0])
+            rect2 = cv2.minAreaRect(rects[1])
+            
+            pose = None
+            if stereoCameraModel is not None: #If we get this, we have(?) stereo camera data, so we can do a full 3d pose
+                mask1 = np.zeros((imageDisparityRect.height, imageDisparityRect.width, 1), np.uint8)
+                roi1Center = (rect1[0][0] + rect1[1][0]*0.4*cos(rect1[3]), rect1[0][0] + rect1[1][1]*0.4*sin(rect1[3]))
+                cv2.circle(mask1, roi1Center, min(rect1[1])*0.5, (255), -1)
+                
+                mask2 = np.zeros((imageDisparityRect.height, imageDisparityRect.width, 1), np.uint8)
+                roi2Center = (rect1[0][0] - rect1[1][0]*0.4*cos(rect1[3]), rect1[0][0] - rect1[1][1]*0.4*sin(rect1[3]))
+                cv2.circle(mask2, roi2Center, min(rect1[1])*0.5, (255), -1)
+                
+                mask3 = np.zeros((imageDisparityRect.height, imageDisparityRect.width, 1), np.uint8)
+                roi3Center = (rect2[0][0] + rect2[1][0]*0.4*cos(rect2[3]), rect2[0][0] + rect2[1][1]*0.4*sin(rect2[3]))
+                cv2.circle(mask3, roi3Center, min(rect2[1])*0.5, (255), -1)
+                
+                mask4 = np.zeros((imageDisparityRect.height, imageDisparityRect.width, 1), np.uint8) 
+                roi4Center = (rect2[0][0] - rect2[1][0]*0.4*cos(rect2[3]), rect2[0][0] - rect2[1][1]*0.4*sin(rect2[3]))
+                cv2.circle(mask4, roi4Center, min(rect2[1])*0.5, (255), -1)
+                
+                disp[1] = cv2.bitwise_and(imageDisparityRect, imageDisparityRect, mask=mask1).max() #determine disparity on all the gate corners
+                disp[2] = cv2.bitwise_and(imageDisparityRect, imageDisparityRect, mask=mask2).max()
+                disp[3] = cv2.bitwise_and(imageDisparityRect, imageDisparityRect, mask=mask3).max()
+                disp[4] = cv2.bitwise_and(imageDisparityRect, imageDisparityRect, mask=mask4).max()
+                
+                if disp.count(0) < 1: #we can still get pose with 3 disparity readings
+                    corner[1] = self.stereoCameraModel.projectPixelTo3d(roi1Center,disp[1]) #Find all 4 gate corners in 3d
+                    corner[2] = self.stereoCameraModel.projectPixelTo3d(roi2Center,disp[2])
+                    corner[3] = self.stereoCameraModel.projectPixelTo3d(roi3Center,disp[3])
+                    corner[4] = self.stereoCameraModel.projectPixelTo3d(roi4Center,disp[4])
+                    
+                    normals = self.normalsFromAllCorners(corner, disp) #generates normals from all the combinations of corners, *should* be the right way round
+                    
+            if pose is None: #no stereo, center position only
+                gatewidth = abs(rect1[0][0] - rect2[0][0])
+                gateheight = abs(rect1[0][1] - rect2[0][1])
+                
+                gatesize = sqrt(gatewidth*gatewidth + gateheight*gateheight)
+                
+                c = 1 #actual gate size constant
+                gateDist = c * (self.cameramodel.fx()*(gatesize/gatewidth)+self.cameramodel.fy()*(gatesize/gateheight))/gatesize
+                #I *think* this accounts for rotational wierdness in the focal lengths
+                
+                gateCenter = ((rect1[0][0] + rect2[0][0])/2, (rect1[0][1] + rect2[0][1])/2)
+                projectedRay = self.cameraModel.projectPixelTo3dRay(gateCenter)
+                estLocation = tuple(gateDist*x for x in projectedRay)
+                
+            return feedback
+        else:
+            return None
+            
+
